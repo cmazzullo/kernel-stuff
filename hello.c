dc07e4176f24a869e708ad12efd78f14fe26dab9
Chris Mazzullo <chris.mazzullo@gmail.com>
2017-10-11 20:55:48 -0400

    Got this compiling!!!

--- hello.c
+++ hello.c
@@ -1,17 +1,17 @@
 #include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
-static int hello_init(void)
+static int __init hello_init(void)
 {
   printk(KERN_ALERT "Hello, world!\n");
   return 0;
 }
 
-static int hello_exit(void)
+static void __exit hello_exit(void)
 {
   printk(KERN_ALERT "Goodbye, cruel world!\n");
-  return 0;
 }
 
 module_init(hello_init);
